06d85e2b0c0d394573b5012eadaecf2a9627c848
uvos <devnull@uvos.xyz>
2021-04-26 13:42:02 +0200

    Add missing charge-mode.sh

---
+++ charge-mode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+font=$(fc-match --format=%{file} DejaVuSans.tff)
+
+xinit /bin/bash -c "charging_sdl -wcreapf $font; echo -n \$? > /var/run/chargingret" -- vt1
+
+retreason=$(cat /var/run/chargingret)
+
+if [ $retreason -lt 0 ]; then
+    echo charging_sdl failed! booting default runlevel
+    openrc default
+elif [ $retreason -eq 0 ]; then
+    echo booting default runlevel
+    openrc default
+elif [ $retreason -eq 1 ]; then
+    echo powering down from charging mode
+    poweroff
+elif [ $retreason -eq 2 ]; then
+    echo booting default runlevel due to rtc alarm
+    openrc default
+fi
